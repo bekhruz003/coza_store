e8b408337399ebd9b59d9d68cdfa31fcae2e4496
Behruz <behruzparpiyev20@gmail.com>
2023-01-09 13:51:28 +0500

    blog detail done and added requirements.txt

--- blogs/views.py
+++ blogs/views.py
@@ -4,10 +4,16 @@ from .models import PostModel
 
 
 class PostListView(ListView):
-    queryset = PostModel.objects.order_by('-id')
     template_name = 'blog.html'
 
+    def get_queryset(self):
+        qs = PostModel.objects.order_by('-id')
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tag__name=tag)
+        return qs
 
-class PostDetailView(ListView):
+
+class PostDetailView(DetailView):
     model = PostModel
     template_name = 'blog-detail.html'
