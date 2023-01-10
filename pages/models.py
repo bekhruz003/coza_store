18e0c62c81103268b8cda67c5f3b206ea46094b1
Behruz <behruzparpiyev20@gmail.com>
2023-01-10 14:39:45 +0500

    home page banner done

--- pages/models.py
+++ pages/models.py
@@ -13,3 +13,19 @@ class ContactModel(models.Model):
     class Meta:
         verbose_name = _('contact')
         verbose_name_plural = _('contacts')
+
+
+class BannerModel(models.Model):
+    collection = models.CharField(max_length=60, verbose_name=_('collection'))
+    title = models.CharField(max_length=60, verbose_name=_('title'))
+    image = models.ImageField(upload_to='banners/', verbose_name=_('image'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created at'))
+    is_active = models.BooleanField(default=False, blank=True, verbose_name=_('is active'))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'banner'
+        verbose_name_plural = 'banners'
+
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, reverse
 from django.views.generic import TemplateView, CreateView
 from .forms import ContactModelForm
+from .models import BannerModel
 
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['banners'] = BannerModel.objects.filter(is_active=True)
+        return data
 
 
 class ContactView(CreateView):
