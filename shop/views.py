f4852501a4ad0e24e5f2a05971371c2fc7ae97f8
Behruz <behruzparpiyev20@gmail.com>
2023-02-24 09:48:41 +0500

    shop page tag and categories done

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import ProductModel, ProductTagModel
+from .models import ProductModel, ProductTagModel, CategoryModel
 
 
 class ShopView(ListView):
@@ -13,11 +13,20 @@ class ShopView(ListView):
         search = self.request.GET.get('search')
         if search:
             qs = qs.filter(title__icontains=search)
+
+        cat = self.request.GET.get('cat')
+        if cat:
+            qs = qs.filter(category_id=cat)
+
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tags=tag)
         return qs
 
     def get_context_data(self, *, object_list=None, **kwargs):
         data = super().get_context_data()
         data['tags'] = ProductTagModel.objects.all()
+        data['categories'] = CategoryModel.objects.all()
         return data
 
 
